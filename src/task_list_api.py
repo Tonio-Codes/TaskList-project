8dea017ea2f2692e560e9c84c87da4c2e683defa
tony <anthonymontano146@gmail.com>
2026-09-26 10:34:22 -0700

    post method to create a task

--- src/task_list_api.py
+++ src/task_list_api.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
-import task_list
+from . import task_list
+from pydantic import BaseModel
+
+class Todo(BaseModel):
+     task: str = ""
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"hello": "world"}
+@app.get("/tasks")
+def get_tasks():
+    return task_list.load_tasks()
+
+@app.post("/tasks")
+def add_task(toDo: Todo):
+     currentTasks = task_list.load_tasks()
+     currentId = task_list.load_id_counter()
+     newIdCounter = task_list.add_task(currentTasks,[toDo.task],currentId)
+
+     task_list.save_tasks(currentTasks)
+     task_list.save_id_counter(newIdCounter)
+
+     return {
+          
+          "id" : currentId,
+          "task": toDo.task,
+          "isFinished": False
+     }
